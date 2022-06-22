e8fcf32d1367bc9efde419b31fd69e1ab4d4b3f3
iamb4uc <iamb4uc@tuta.io>
2022-06-23 01:31:12 +0530

    alpha added

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=17:antialias=true:autohint=true";
+static char *font = "RobotoMono Nerd Font:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
